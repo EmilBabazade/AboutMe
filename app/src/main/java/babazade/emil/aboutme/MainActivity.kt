2ff0a5b3ee345da2ce7805e35d13866074f3174c
emil <emilbabazade29@gmail.com>
2020-11-22 16:01:31 +0200

    initial commit

---
+++ app/src/main/java/babazade/emil/aboutme/MainActivity.kt
@@ -0,0 +1,10 @@
+package babazade.emil.aboutme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
